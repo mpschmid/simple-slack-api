8c7efc2ff628a853b752e6b1669bb8e9ac41bc13
George Simpson <gsimpson@netflix.com>
2019-01-20 10:45:27 -0800

    Add parsing for member joined channel event

--- sources/src/main/java/com/ullink/slack/simpleslackapi/events/SlackMemberJoinedChannel.java
+++ sources/src/main/java/com/ullink/slack/simpleslackapi/events/SlackMemberJoinedChannel.java
@@ -1,12 +1,22 @@
 package com.ullink.slack.simpleslackapi.events;
 
 import com.ullink.slack.simpleslackapi.SlackChannel;
+import com.ullink.slack.simpleslackapi.SlackTeam;
+import com.ullink.slack.simpleslackapi.SlackUser;
+import lombok.Data;
 import lombok.NonNull;
 
+@Data
 public class SlackMemberJoinedChannel implements SlackEvent{
 
     @NonNull
     private SlackChannel slackChannel;
+
+    @NonNull
+    private SlackUser slackUserJoining;
+
+    @NonNull
+    private SlackUser slackUserInviter;
 
     @Override
     public SlackEventType getEventType() {
